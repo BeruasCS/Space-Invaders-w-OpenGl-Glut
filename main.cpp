77f525557083d5c232c6b28e612193b28714b0e9
HeullerRamos <heullervieira4@gmail.com>
2025-11-21 18:53:14 -0300

    main

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include <GL/glut.h>
+#include "JogoPrincipal.h"  // Novo nome
+#include "Constantes.h"     // Novo nome
+
+// Instância global renomeada
+JogoPrincipal g_jogo;
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    g_jogo.render(); // Renderiza o JogoPrincipal
+    
+    glutSwapBuffers();
+}
+
+void update(int value) {
+    g_jogo.update(); // Atualiza o JogoPrincipal
+
+    glutTimerFunc(1000 / FRAMES_POR_SEGUNDO, update, 0);
+    glutPostRedisplay();
+}
+
+void handleKeysDown(unsigned char key, int x, int y) {
+    g_jogo.processarTecla(key, true);
+}
+
+void handleKeysUp(unsigned char key, int x, int y) {
+    g_jogo.processarTecla(key, false);
+}
+
+void handleSpecialKeysDown(int key, int x, int y) {
+    g_jogo.processarTeclaEspecial(key, true);
+}
+
+void handleSpecialKeysUp(int key, int x, int y) {
+    g_jogo.processarTeclaEspecial(key, false);
+}
+
+void reshape(int w, int h) {
+    if (h == 0) h = 1;
+    glViewport(0, 0, w, h);
+    
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, LARGURA_JANELA, 0.0, ALTURA_JANELA);
+    
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void Init(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(LARGURA_JANELA, ALTURA_JANELA);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("TP03 - Space Invaders (Modificado)");
+    
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+int main(int argc, char** argv) {
+    Init(argc, argv);
+    
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutTimerFunc(1000 / FRAMES_POR_SEGUNDO, update, 0);
+    
+    glutKeyboardFunc(handleKeysDown);
+    glutKeyboardUpFunc(handleKeysUp);
+    glutSpecialFunc(handleSpecialKeysDown);
+    glutSpecialUpFunc(handleSpecialKeysUp);
+    
+    g_jogo.init();
+    
+    glutMainLoop();
+    return 0;
+}
